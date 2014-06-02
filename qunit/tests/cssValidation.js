d81b4802deb3ee8c5d7dedbf61dd94248cb2fa01
Seanna Musgrave <seannamusgrave@gmail.com>
2014-06-02 14:28:09 -0700

    Removed second arguement from qunit tests (Not needed due to all having the same expectation)

--- qunit/tests/cssValidation.js
+++ qunit/tests/cssValidation.js
@@ -23,37 +23,37 @@
   test("Permits entries from easy list", function()
   {
     var tests = [
-      ['rachaelrayshow.com##div[style="margin: 5px auto 0 auto; padding: 0px; color: #999999; font-size: 10px; text-align: center;"]', true],
-      ['||21sexturycash.com^$third-party', true],
-      ['##.advert_back_300xXXX', true],
-      ['mail.google.com##.aeF > .nH > .nH[role="main"] > .aKB', true],
-      ['cpu-world.com##div[style="height: 90px; padding: 3px; text-align: center"]', true],
-      ['@@||sascdn.com^*/jwplayer-plugin.swf?$object-subrequest', true],
-      ['##[onclick^="window.open(\'http://adultfriendfinder.com/search/"]', true],
-      ['fulldls.com###table_filter + .torrent_table', true],
-      ['movie2k.tl###tablemoviesindex > tbody:first-child:last-child > tr:last-child', true],
-      ['movie4k.to###tablemoviesindex:last-child > tbody:first-child:last-child > tr:last-child', true]
+      ['rachaelrayshow.com##div[style="margin: 5px auto 0 auto; padding: 0px; color: #999999; font-size: 10px; text-align: center;"]'],
+      ['||21sexturycash.com^$third-party'],
+      ['##.advert_back_300xXXX'],
+      ['mail.google.com##.aeF > .nH > .nH[role="main"] > .aKB'],
+      ['cpu-world.com##div[style="height: 90px; padding: 3px; text-align: center"]'],
+      ['@@||sascdn.com^*/jwplayer-plugin.swf?$object-subrequest'],
+      ['##[onclick^="window.open(\'http://adultfriendfinder.com/search/"]'],
+      ['fulldls.com###table_filter + .torrent_table'],
+      ['movie2k.tl###tablemoviesindex > tbody:first-child:last-child > tr:last-child'],
+      ['movie4k.to###tablemoviesindex:last-child > tbody:first-child:last-child > tr:last-child']
     ];
 
     for (var i = 0; i < tests.length; i++)
-      equal(validFilter(tests[i][0]), tests[i][1], tests[i][0]);
+      equal(validFilter(tests[i][0]), true, tests[i][0]);
   });
 
   test("Catches CSS Errors", function()
   {
     var tests = [
-      ['##.box[', false],
-      ['##.box[target="', false],
-      ['##.box[target="foo\'', false],
-      ['##.box[target=\'foo"', false],
-      ['##.box[target="foo"', false],
-      ['##:bogus', false],
-      ['##.box {', false],
-      ['##.box {target="foo"}', false],
+      ['##.box['],
+      ['##.box[target="'],
+      ['##.box[target="foo\''],
+      ['##.box[target=\'foo"'],
+      ['##.box[target="foo"'],
+      ['##:bogus'],
+      ['##.box {'],
+      ['##.box {target="foo"}'],
     ];
 
     for (var i = 0; i < tests.length; i++)
-      equal(validFilter(tests[i][0]), tests[i][1], tests[i][0]);
+      equal(validFilter(tests[i][0]), false, tests[i][0]);
   });
 
 
